270ecfe71f77373643340e3a5fbb13413c4756fa
TariqNazmusSadat <tariq.sadat@northsouth.edu>
2020-08-21 15:30:49 +0600

    add routes/contacts ,auth,users

--- server.js
+++ server.js
@@ -8,5 +8,10 @@ app.get('/',
     msg: "connected"
   })
 );
+//Define routes
+app.use('/api/users',require('./routes/users'));
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/contacts',require('./routes/contacts'));
+
 
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
