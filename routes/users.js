581da3ba8f668e9626114e239bf42a1d55f9e902
TariqSadat <sadat550@gmail.com>
2020-10-14 23:35:00 +0600

    rollback on users.js

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,58 @@
-const mongoose = require('mongoose');
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
-const ContactSchema = mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'users'
-  },
-  name: {
-    type: String,
-    required: true
-  },
-    email: {
-    type: String,
-    required: true
-  },
-    phone: {
-    type: String
-  },
-  type:{
-    type: String,
-    default: 'personal'
-  },
-    date: {
-    type: Date,
-    default: Date.now
+
+//@route    POST  api/users
+//des       register n user
+//access    public
+router.post('/',[
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please input Proper email').not().isEmpty(),
+  check('password', 'Password too weak.Please select a password with 6 or more character.').isLength({ min:6 })
+],async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { name, email, password } = req.body;
+
+  try{
+    let user = await User.findOne({ email });
+    if(user){
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    user = new User({
+      name, email, password
+    });
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+    const payload = {
+      user: {
+        id : user.id
+      }
+    }
+    jwt.sign(
+      payload,
+       config.get('jwtSecret'),
+       {expiresIn : 3600000},
+       (err, token) => {
+         if(err) throw err;
+         res.json({ token })
+       });
+  }catch(err){
+      console.error(err.msg);
+      res.status(500).send('server error');
   }
 });
 
-module.exports = mongoose.model('contact',ContactSchema);
+module.exports = router;
